533997365e93a54267e34d2e5002422f890e4c5a
viktor19764 <czaass@gmail.com>
2018-12-15 04:13:09 +0200

    fixed typo on string 4 (brackets)

--- src/lesson10/abstractbigexample/DeveloperEmployee.java
+++ src/lesson10/abstractbigexample/DeveloperEmployee.java
@@ -1,7 +1,7 @@
 package lesson10.abstractbigexample;
 
 public class DeveloperEmployee extends Employee{
-    private String [] frameworks = new String[[10]];
+    private String [] frameworks = new String[10];
 
     @Override
     void paySalary() {
