c1aa83a067c4008360e5885334f55f1103e9f46b
viktor19764 <czaass@gmail.com>
2018-12-15 02:45:13 +0200

    added constructors

--- src/lesson10/abstractexample/MySQLProvider.java
+++ src/lesson10/abstractexample/MySQLProvider.java
@@ -2,6 +2,10 @@ package lesson10.abstractexample;
 
 public class MySQLProvider extends DbProvider{
 
+    public MySQLProvider(String dbHost) {
+        super(dbHost);
+    }
+
     @Override
     void connectToDb() {
 
--- src/lesson10/abstractexample/OracleDbProvider.java
+++ src/lesson10/abstractexample/OracleDbProvider.java
@@ -2,6 +2,10 @@ package lesson10.abstractexample;
 
 public class OracleDbProvider extends DbProvider{
 
+    public OracleDbProvider(String dbHost) {
+        super(dbHost);
+    }
+
     @Override
     void connectToDb() {
 
