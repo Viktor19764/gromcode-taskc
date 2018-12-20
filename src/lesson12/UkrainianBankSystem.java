7bdcb325a966d28bc49d8e15f0c6af22210ebc70
viktor19764 <czaass@gmail.com>
2018-12-20 09:39:05 +0200

    changed paySalary()

--- src/lesson12/UkrainianBankSystem.java
+++ src/lesson12/UkrainianBankSystem.java
@@ -1,77 +1,74 @@
 package lesson12;
 
 public class UkrainianBankSystem implements BankSystem {
-    //private String withdrawalEroMsq = "Can`t withdraw money " + amount + " from user " + user.toString()
 
     @Override
     public void withdraw(User user, int amount) {
-//        int limitOfWithdrawal = user.getBank().getLimitOfFunding();
-//        if (amount + user.getBank().getCommission(amount)> limitOfWithdrawal){
-//            printWithdrawalEroMsq(user, amount);
-//            return;
-//        }
-//        if (amount + user.getBank().getCommission(amount)> user.getBalance()){
-//            printWithdrawalEroMsq(user, amount);
-//            return;
-//        }
-        if (!checkWithdraw(user, amount))
+        //перевірити чи можна зняти
+        // перевірити ліміт
+        // перевірити чи досить грошей
+        //зняти гроші
+
+        if (!checkWithdraw(user, amount)) {
+            printWithdrawalErrorMsg(amount, user);
             return;
+        }
+
         user.setBalance(user.getBalance() - amount - amount * user.getBank().getCommission(amount));
     }
 
     @Override
     public void fund(User user, int amount) {
-        if (!checkFund(user, amount))
+        if (!checkFunding(user, amount))
             return;
         user.setBalance(user.getBalance() + amount);
     }
 
     @Override
     public void transferMoney(User fromUser, User toUser, int amount) {
+        //знімаємо гроші з fromUser
+        //поповнюємо toUser
         if (!checkWithdraw(fromUser, amount))
             return;
-        if (!checkFund(toUser, amount))
+        if (!checkFunding(toUser, amount))
             return;
         if (fromUser.getBank().getCurrency() == toUser.getBank().getCurrency()) {
             withdraw(fromUser, amount);
+            //if (!checkWithdraw(fromUser, amount))
+            //return;
+
+            //fromUser.setBalance(fromUser.getBalance() - amount - amount * fromUser.getBank().getCommission(amount));
+
             fund(toUser, amount);
         } else
-            System.out.println("FATAL ERROR!!! TRANSACTION NOT GOING!!! INDIAN CODE NOT WORKING");
+            System.out.println("Transfer error");
     }
 
     @Override
     public void paySalary(User user) {
-        if (!checkFund(user, user.getSalary()))
+        if(!checkFunding(user, user.getSalary()))
             return;
         fund(user, user.getSalary());
     }
 
     private void printWithdrawalErrorMsg(int amount, User user) {
-        System.err.println("Can't withdraw money " + amount + " from user" + user.toString());
-    }
-
-    private void printFundErrorMsg(int amount, User user) {
-        System.err.println("Can't fund money " + amount + " to user" + user.toString());
-    }
-
-    private boolean checkFund(User user, int amount) {
-        if (amount + user.getBank().getCommission(amount) > user.getBank().getLimitOfFunding()) {
-            printFundErrorMsg(amount, user);
-            return false;
-        }
-        return true;
+        System.err.println("Can't withdraw money " + amount + " from user " + user.toString());
     }
 
     private boolean checkWithdraw(User user, int amount) {
-        return checkWithdrawLimits(user, amount, user.getBank().getLimitOfWithdrawal()) &&
-                checkWithdrawLimits(user, amount, user.getBalance());
+        return checkTransferLimits(user, amount, user.getBank().getLimitOfWithdrawal()) && checkTransferLimits(user, amount, user.getBalance());
     }
 
-    private boolean checkWithdrawLimits(User user, int amount, double limit) {
-        if (amount + user.getBank().getCommission(amount) > limit) {
-            printWithdrawalErrorMsg(amount, user);
+    private boolean checkTransferLimits(User user, int amount, double limit) {
+        if (amount + amount * user.getBank().getCommission(amount) > limit) {
             return false;
         }
         return true;
     }
+
+    private boolean checkFunding(User user, int amount) {
+        return user.getBank().getLimitOfFunding() >= amount ? true: false;
+        //return checkTransferLimits(user, amount, user.getBank().getLimitOfFunding());
+    }
+
 }
