0b86b8a3721a91b2ece9eea932955e3a0dad55c0
viktor19764 <czaass@gmail.com>
2018-12-31 06:06:17 +0200

    some changes

--- src/lesson15/HW/Controller.java
+++ src/lesson15/HW/Controller.java
@@ -26,8 +26,8 @@ public class Controller {
         for (API api : apis) {
             if (api != null) {
                 for (Room el : api.getAll()) {
-                    //if (el != null && price == el.getPrice() && persons == el.getPersons() && city == el.getCityName() && hotel == el.getHotelName()) {
-                    if (el != null && el.equals(room)){
+                    if (el != null && price == el.getPrice() && persons == el.getPersons() && city == el.getCityName() && hotel == el.getHotelName()) {
+                    //if (el != null && el.equals(room)){
                         appropriateRooms[b] = el;
                         b++;
                     }
